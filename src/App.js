471dac28cbda36b41c1fc8f5901be660846a0f64
devCecy <im.ceciliaan@gmail.com>
2023-01-12 23:47:29 +0900

    test : red-> MidnightVioletRed, blue->MidnightBlue로 변경 & 상수화

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState } from "react";
 
+export const MidnightBlue = "MidnightBlue";
+export const MidnightVioletRed = "MidnightVioletRed";
 /**
  * 카멜케이스의 대문자 앞에 공백을 넣어줍니다.
  * @param {*} colorName
@@ -11,9 +13,10 @@ export const replaceCamelWithSpaces = (colorName) => {
 };
 
 function App() {
-	const [btnColor, setBtnColor] = useState("red");
+	const [btnColor, setBtnColor] = useState(MidnightVioletRed);
 	const [isChecked, setIsChecked] = useState(false);
-	const newBtnColor = btnColor === "red" ? "blue" : "red";
+	const newBtnColor =
+		btnColor === MidnightVioletRed ? MidnightBlue : MidnightVioletRed;
 
 	return (
 		<div>
@@ -22,7 +25,7 @@ function App() {
 				style={{ backgroundColor: isChecked ? "gray" : btnColor }}
 				onClick={() => setBtnColor(newBtnColor)}
 			>
-				Change to {newBtnColor}
+				Change to {replaceCamelWithSpaces(newBtnColor)}
 			</button>
 			<label htmlFor="disabled-button-checkbox">Disable button</label>;
 			<input
--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,28 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
+import App, { MidnightBlue, MidnightVioletRed } from "./App";
 
 import { replaceCamelWithSpaces } from "./App";
 
 test("버튼의 초기 색상이 올바른지 확인하고, 버튼 클릭 시 업데이트합니다", () => {
 	render(<App />);
-	const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+	const colorBtn = screen.getByRole("button", {
+		name: "Change to Midnight Blue",
+	});
 
-	expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
+	expect(colorBtn).toHaveStyle({ backgroundColor: MidnightVioletRed });
 
 	// 버튼을 클릭했을 때,
 	fireEvent.click(colorBtn);
-	expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
-	expect(colorBtn).toHaveTextContent("Change to red");
+	expect(colorBtn).toHaveStyle({ backgroundColor: MidnightBlue });
+	expect(colorBtn).toHaveTextContent("Change to Midnight Violet Red");
 });
 
 test("체크박스의 초기조건을 테스트합니다.", () => {
 	// 체크박스의 초기조건은 활성화 상태이다.
 	render(<App />);
-	const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+	const colorBtn = screen.getByRole("button", {
+		name: "Change to Midnight Blue",
+	});
 	expect(colorBtn).toBeEnabled();
 
 	const checkbox = screen.getByRole("checkbox");
@@ -30,7 +34,9 @@ test("체크박스의 상태에따라 버튼의 활성화 여부가 결정됩니
 
 	// 체크박스가 체크되면, 버튼은 비활성화된다.
 	const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
-	const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+	const colorBtn = screen.getByRole("button", {
+		name: "Change to Midnight Blue",
+	});
 
 	fireEvent.click(checkbox);
 	expect(colorBtn).toBeDisabled();
@@ -43,24 +49,26 @@ test("체크박스의 상태에따라 버튼의 활성화 여부가 결정됩니
 test("버튼 비활성화 시 버튼 색은 gray로 바뀐다.", () => {
 	render(<App />);
 	const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
-	const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+	const colorBtn = screen.getByRole("button", {
+		name: "Change to Midnight Blue",
+	});
 
 	// 버튼이 비활성화되면, 버튼색은 gray로 변한다.
 	fireEvent.click(checkbox);
-	expect(colorBtn).toHaveStyle({ backgroundColor: "gray" });
+	expect(colorBtn).toHaveStyle({ backgroundColor: MidnightVioletRed });
 
-	// 버튼이 활성화되면, 버튼색은 red로 변한다.
+	// 버튼이 활성화되면, 버튼색은 MidnightVioletRed로 변한다.
 	fireEvent.click(checkbox);
-	expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
+	expect(colorBtn).toHaveStyle({ backgroundColor: MidnightVioletRed });
 
 	// 버튼을 클릭해서 색을 변경하고, 버튼을 비활성화시키면 버튼이 gray가 된다.
 	fireEvent.click(colorBtn);
 	fireEvent.click(checkbox);
 	expect(colorBtn).toHaveStyle({ backgroundColor: "gray" });
 
-	// 다시 버튼을 활성화시키면, 버튼색은 blue다
+	// 다시 버튼을 활성화시키면, 버튼색은 MidnightBlue이다.
 	fireEvent.click(checkbox);
-	expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
+	expect(colorBtn).toHaveStyle({ backgroundColor: MidnightBlue });
 });
 
 describe("카멜케이스의 대문자 앞에 스페이스가 생성됩니다.", () => {
@@ -69,11 +77,11 @@ describe("카멜케이스의 대문자 앞에 스페이스가 생성됩니다.",
 	});
 
 	test("대문자가 하나일 경우", () => {
-		expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+		expect(replaceCamelWithSpaces(MidnightBlue)).toBe("Midnight Blue");
 	});
 
 	test("대문자가 여러개인 경우", () => {
-		expect(replaceCamelWithSpaces("MidnightVioletRed")).toBe(
+		expect(replaceCamelWithSpaces(MidnightVioletRed)).toBe(
 			"Midnight Violet Red"
 		);
 	});
