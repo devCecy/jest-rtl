bbe7526094d8eec3a8fd4a31c7c603e31826ebdf
devCecy <im.ceciliaan@gmail.com>
2023-01-12 22:37:08 +0900

    test : 버튼 클릭 시 스타일&문구 변경

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("버튼의 초기 색상이 올바른지 확인하고, 버튼 클릭 시 업데이트합니다", () => {
+	render(<App />);
+	const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+
+	expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
+
+	// 버튼을 클릭했을 때,
+	fireEvent.click(colorBtn);
+	expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
+	expect(colorBtn).toHaveTextContent("Change to red");
 });
